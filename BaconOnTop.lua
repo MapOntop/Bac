73af8539a121659079548b6b8f30b933e49126ed
MapOntop <157943459+MapOntop@users.noreply.github.com>
2024-01-28 20:27:15 +0700

    BaconOnTop.lua

--- BaconOnTop.lua
+++ BaconOnTop.lua
@@ -2310,9 +2310,9 @@ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 ImageButton.Parent = ScreenGui
 ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageButton.Position = UDim2.new(0.10615778, 0, 0.16217947, 0)
-ImageButton.Size = UDim2.new(0.0627121851, 0, 0.107579626, 0)
-ImageButton.Image = "rbxassetid://16150579788"
+ImageButton.Position = UDim2.new(255,215,0)
+ImageButton.Size = UDim2.new(255,215,0)
+ImageButton.Image = "rbxassetid://15843603697"
 
 UICorner.CornerRadius = UDim.new(0, 30)
 UICorner.Parent = ImageButton
